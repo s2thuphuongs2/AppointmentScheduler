c765f60f3c27f5656f27e13fcbdaf0f4ca2e48ba
BookStore <s2thuphuongs2@gmail.com>
2023-12-25 19:52:01 +0700

    Correction project

--- src/main/java/com/example/appointmentscheduler/AppointmentSchedulerApplication.java
+++ src/main/java/com/example/appointmentscheduler/AppointmentSchedulerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
+
 @EnableScheduling
 @SpringBootApplication
 public class AppointmentSchedulerApplication {
